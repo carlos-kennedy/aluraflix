e9235a2f0d4f5393dbdf38e36f3b5a91e6bf9a74
carlos-kennedy <carlosjustcarlos@outlook.com>
2025-01-14 14:17:40 -0300

    Deploy json-server to render

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Nome do seu arquivo JSON
-const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
-server.listen(PORT, () => {
-  console.log("JSON Server is running");
+app.get("/", (req, res) => {
+  res.send("Hello, Render!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });

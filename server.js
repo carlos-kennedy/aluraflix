3424cb11f8a02ebc9a5ec35cf49ab87e0d171879
carlos-kennedy <carlosjustcarlos@outlook.com>
2025-01-14 14:56:58 -0300

    Deploy json-server to package.json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Altere para o seu arquivo JSON
 const middlewares = jsonServer.defaults();
